e2321a57200824bc28f87e6ae5d8e3d020006245
jungyu333 <jungyu.kim@partridgesystems.com>
2025-03-01 13:36:45 +0900

    feat : auth Layout UI

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -1,16 +1,26 @@
-import { Stack } from 'expo-router';
+import { Link, Stack } from 'expo-router';
 import { Foundation } from '@expo/vector-icons';
 import { colors } from '@/constants/color';
 
 const AuthLayout = () => {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerTintColor: colors.BLACK,
+        contentStyle: {
+          backgroundColor: colors.WHITE,
+        },
+      }}>
       <Stack.Screen
         name='index'
         options={{
           title: '로그인',
           headerShown: true,
-          headerLeft: () => <Foundation name={'home'} size={28} color={colors.BLACK} />,
+          headerLeft: () => (
+            <Link href={'/'} replace>
+              <Foundation name={'home'} size={28} color={colors.BLACK} />{' '}
+            </Link>
+          ),
         }}
       />
     </Stack>
