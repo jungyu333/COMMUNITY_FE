fe5b1bc7e1f4420c2eef4c0992a6242f0787566e
jungyu333 <jungyu.kim@partridgesystems.com>
2025-03-01 13:45:38 +0900

    feat : Create Auth Screen UI

--- app/auth/index.tsx
+++ app/auth/index.tsx
@@ -1,11 +1,49 @@
-import { SafeAreaView, Text } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { CustomButton } from '@/components/CustomButton';
+import { Link } from 'expo-router';
 
 const AuthScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>인증 홈 스크린</Text>
+    <SafeAreaView style={Styles.container}>
+      <View style={Styles.imageContainer}>
+        <Image source={require('@/assets/images/logo.png')} style={Styles.logo} />
+      </View>
+      <View style={Styles.buttonContainer}>
+        <CustomButton label='이메일 로그인' />
+        <Link href='/' style={Styles.signupText}>
+          이메일로 가입하기
+        </Link>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default AuthScreen;
+
+const Styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  imageContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 2,
+  },
+
+  buttonContainer: {
+    paddingHorizontal: 32,
+    flex: 1,
+  },
+
+  logo: {
+    width: 112,
+    height: 112,
+  },
+
+  signupText: {
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+    marginTop: 20,
+  },
+});
