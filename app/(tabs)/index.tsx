2de552b081a531cfa9185f3cd8900cfbdcc03896
jungyu333 <jungyu.kim@partridgesystems.com>
2025-03-02 12:38:16 +0900

    feat : Add FeedList.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,10 @@
 import { SafeAreaView } from 'react-native';
-import { FeedItem } from '@/components/FeedItem';
+import { FeedList } from '@/components/FeedList';
 
 export default function HomeScreen() {
   return (
     <SafeAreaView>
-      <FeedItem
-        post={{
-          id: 1,
-          userId: 1,
-          title: 'title',
-          description:
-            '더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.',
-          createdAt: 'createdAt',
-          author: {
-            id: 1,
-            nickname: 'nickname',
-            imageUri: '',
-          },
-          imageUris: [],
-          likes: [{ userId: 1 }],
-          hasVote: false,
-          voteCount: 1,
-          commentCount: 1,
-          viewCount: 1,
-        }}
-      />
+      <FeedList />
     </SafeAreaView>
   );
 }
---
+++ components/FeedList.tsx
@@ -0,0 +1,102 @@
+import { FlatList, StyleSheet } from 'react-native';
+import { FeedItem } from '@/components/FeedItem';
+import { colors } from '@/constants/color';
+
+const dummyData = [
+  {
+    id: 1,
+    userId: 1,
+    title: 'title',
+    description:
+      '더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.',
+    createdAt: 'createdAt',
+    author: {
+      id: 1,
+      nickname: 'nickname',
+      imageUri: '',
+    },
+    imageUris: [],
+    likes: [{ userId: 1 }],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+  {
+    id: 2,
+    userId: 1,
+    title: 'title',
+    description:
+      '더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.',
+    createdAt: 'createdAt',
+    author: {
+      id: 1,
+      nickname: 'nickname',
+      imageUri: '',
+    },
+    imageUris: [],
+    likes: [{ userId: 1 }],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+  {
+    id: 3,
+    userId: 1,
+    title: 'title',
+    description:
+      '더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.',
+    createdAt: 'createdAt',
+    author: {
+      id: 1,
+      nickname: 'nickname',
+      imageUri: '',
+    },
+    imageUris: [],
+    likes: [{ userId: 1 }],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+
+  {
+    id: 4,
+    userId: 1,
+    title: 'title',
+    description:
+      '더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.더미 내용입니다.',
+    createdAt: 'createdAt',
+    author: {
+      id: 1,
+      nickname: 'nickname',
+      imageUri: '',
+    },
+    imageUris: [],
+    likes: [{ userId: 1 }],
+    hasVote: false,
+    voteCount: 1,
+    commentCount: 1,
+    viewCount: 1,
+  },
+];
+
+export const FeedList = () => {
+  return (
+    <FlatList
+      data={dummyData}
+      renderItem={({ item }) => <FeedItem post={item} />}
+      keyExtractor={(item) => String(item.id)}
+      contentContainerStyle={Styles.contentContainer}
+    />
+  );
+};
+
+const Styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 12,
+    backgroundColor: colors.GRAY['200'],
+    gap: 12,
+  },
+});
