6341c61a618462b5ca7072f45c378dedee21fb4b
jungyu333 <jungyu.kim@partridgesystems.com>
2025-03-02 13:53:07 +0900

    style : Input Error Style

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -4,15 +4,17 @@ import { colors } from '@/constants/color';
 interface Props extends TextInputProps {
   label?: string;
   variant?: 'filled' | 'standard' | 'outlined';
+  error?: string;
 }
 
-export const InputField = ({ label, variant = 'filled', ...props }: Props) => {
+export const InputField = ({ label, variant = 'filled', error = '', ...props }: Props) => {
   return (
     <View>
       {label && <Text style={Styles.label}>{label}</Text>}
-      <View style={[Styles.container, Styles[variant]]}>
+      <View style={[Styles.container, Styles[variant], Boolean(error) && Styles.inputError]}>
         <TextInput placeholderTextColor={colors.GRAY['500']} style={Styles.input} {...props} />
       </View>
+      {Boolean(error) && <Text style={Styles.error}>{error}</Text>}
     </View>
   );
 };
@@ -45,5 +47,15 @@ const Styles = StyleSheet.create({
     fontSize: 16,
     padding: 0,
     flex: 1,
+  },
+
+  error: {
+    fontSize: 12,
+    marginTop: 5,
+    color: colors.RED['500'],
+  },
+
+  inputError: {
+    backgroundColor: colors.RED['100'],
   },
 });
