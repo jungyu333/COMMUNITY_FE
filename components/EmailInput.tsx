8f2892dcc348d5f6dfd5b68d44852ed06b63c7e8
jungyu333 <jungyu.kim@partridgesystems.com>
2025-03-04 21:54:04 +0900

    feat : Add EmailInput.tsx Validation

--- components/EmailInput.tsx
+++ components/EmailInput.tsx
@@ -5,12 +5,23 @@ export const EmailInput = () => {
   const { control } = useFormContext();
   return (
     <Controller
-      render={({ field: { onChange, value } }) => (
+      rules={{
+        validate: (data: string) => {
+          if (data.length === 0) {
+            return '이메일을 입력해주세요.';
+          }
+          if (!/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i.test(data)) {
+            return '올바른 이메일 형식이 아닙니다.';
+          }
+        },
+      }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
         <InputField
-          label={'이메일 로그인'}
+          label={'이메일'}
           placeholder={'이메일을 입력해주세요.'}
           value={value}
           onChangeText={onChange}
+          error={error?.message}
         />
       )}
       name={'email'}
