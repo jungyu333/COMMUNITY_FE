913d01ac1ca2e23b5448adb263e70ee8e8f52730
jungyu333 <jungyu.kim@partridgesystems.com>
2025-03-04 21:56:22 +0900

    feat : Add PasswordInput.tsx Validation

--- components/PasswordInput.tsx
+++ components/PasswordInput.tsx
@@ -5,8 +5,22 @@ export const PasswordInput = () => {
   const { control } = useFormContext();
   return (
     <Controller
-      render={({ field: { onChange, value } }) => (
-        <InputField label={'비밀번호'} placeholder={'비밀번호를 입력해주세요.'} value={value} onChangeText={onChange} />
+      rules={{
+        validate: (data: string) => {
+          if (data.length < 8) {
+            return '비밀번호는 8자 이상 입력해주세요.';
+          }
+        },
+      }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
+        <InputField
+          label={'비밀번호'}
+          placeholder={'비밀번호를 입력해주세요.'}
+          secureTextEntry
+          value={value}
+          onChangeText={onChange}
+          error={error?.message}
+        />
       )}
       name={'password'}
       control={control}
