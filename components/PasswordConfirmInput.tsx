50598f51aa79dd4b57e841c274266178c0f1b21b
jungyu333 <jungyu.kim@partridgesystems.com>
2025-03-04 22:04:02 +0900

    feat : Add PasswordConfirmInput.tsx Validation

--- components/PasswordConfirmInput.tsx
+++ components/PasswordConfirmInput.tsx
@@ -1,16 +1,27 @@
-import { Controller, useFormContext } from 'react-hook-form';
+import { Controller, useFormContext, useWatch } from 'react-hook-form';
 import { InputField } from '@/components/InputField';
 
 export const PasswordConfirmInput = () => {
   const { control } = useFormContext();
+  const password = useWatch({ control, name: 'password' });
+
   return (
     <Controller
-      render={({ field: { onChange, value } }) => (
+      rules={{
+        validate: (data: string) => {
+          if (data !== password) {
+            return '비밀번호가 일치하지 않습니다.';
+          }
+        },
+      }}
+      render={({ field: { onChange, value }, fieldState: { error } }) => (
         <InputField
           label={'비밀번호 확인'}
           placeholder={'비밀번호를 입력해주세요.'}
           value={value}
+          secureTextEntry
           onChangeText={onChange}
+          error={error?.message}
         />
       )}
       name={'passwordConfirm'}
